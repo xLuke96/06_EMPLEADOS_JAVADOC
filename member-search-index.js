3ea79376f871cf0ae8b8a45ac7131bcfaccbec8f
Luca Morlando <lucam4523@gmail.com>
2022-12-08 17:15:47 +0100

    commit inicial

---
+++ member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"empleado","c":"Empleado","l":"Empleado()","u":"%3Cinit%3E()"},{"p":"empleado","c":"Empleado","l":"Empleado(String, String, int, double)","u":"%3Cinit%3E(java.lang.String,java.lang.String,int,double)"},{"p":"empleado","c":"Empleado","l":"plus(double)"}];updateSearchResults();
